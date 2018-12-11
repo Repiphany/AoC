3a6e8dd2f10885fa1af964c680a938390f3049ab
Repiphany <45775878+Repiphany@users.noreply.github.com>
2018-12-11 14:22:21 +1300

    Solution for day 2, part 2

--- 2018/day_2/main.py
+++ 2018/day_2/main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import collections
+import itertools
 
 def part_1(ids):
     twins = triplets = 0
@@ -12,8 +13,23 @@ def part_1(ids):
             triplets += 1
     print(twins*triplets)
 
+def string_match(s1, s2):
+    matching = []
+    for l1, l2 in zip(s1, s2):
+        if l1 == l2:
+            matching.append(l1)
+    return ''.join(matching), len(s1) - len(matching)
+
+def part_2(ids):
+    for a, b in itertools.combinations(ids, 2):
+        matching, dl = string_match(a, b)
+        if dl == 1:
+            print(matching)
+            return
+
 if __name__ == '__main__':
     with open('input', 'r') as f:
         ids = [line.strip() for line in f]
     part_1(ids)
+    part_2(ids)
 
