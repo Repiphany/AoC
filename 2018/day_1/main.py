b881814d203d87b2653f3be6ff9d8ca1574fc3a1
Repiphany <45775878+Repiphany@users.noreply.github.com>
2018-12-11 14:05:12 +1300

    Solution for day 2, part 2

--- 2018/day_1/main.py
+++ 2018/day_1/main.py
@@ -6,7 +6,18 @@ def part_1(df):
     f_final = sum(df)
     print(f_final)
 
+def part_2(df):
+    f_seen = set([0])
+    f = 0
+    for i in itertools.cycle(df):
+        f += i
+        if f in f_seen:
+            break
+        f_seen.add(f)
+    print(f)
+
 if __name__ == '__main__':
     with open('input', 'r') as f:
         df = [int(i) for i in f]
     part_1(df)
+    part_2(df)
