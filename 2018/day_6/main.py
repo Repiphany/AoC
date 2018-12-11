59244440304bc9fc26d24df422557cf394c78a7b
Repiphany <45775878+Repiphany@users.noreply.github.com>
2018-12-11 16:52:10 +1300

    Solution for day 6, part 2

--- 2018/day_6/main.py
+++ 2018/day_6/main.py
@@ -20,9 +20,18 @@ def part_1(coordinates):
     areas = [np.sum(grid == i) for i in set(grid.flatten()) - infinite]
     print(sorted(areas)[-1])
     
+def part_2(coordinates, safe = 10000):
+    bounds = np.max(coordinates, axis = 0)
+    grid = np.empty(tuple(bounds), dtype = int)
+    for i, _ in np.ndenumerate(grid):
+        total_mh_dist = np.sum(np.absolute(coordinates - i))
+        grid[i] = total_mh_dist
+    print(np.sum(grid < safe))
+    
 if __name__ == '__main__':
     with open('input', 'r') as f:
         coordinates = np.asarray([tuple(int(i) for i in line.split(', '))
                 for line in f])
     part_1(coordinates)
+    part_2(coordinates)
 
