dbb344111c8dc335041eee2be421e99b9027a30c
Repiphany <45775878+Repiphany@users.noreply.github.com>
2018-12-11 15:41:54 +1300

    Solution for day 4, part 2

--- 2018/day_4/main.py
+++ 2018/day_4/main.py
@@ -19,6 +19,22 @@ def part_1(log):
     m = minutes.most_common()[0][0]
     print(m*most_asleep)
 
+def part_2(log):
+    sleep_tracker = collections.defaultdict(collections.Counter)
+    for line in log:
+        timestamp, action = line.split('] ')
+        if action.startswith('G'):
+            active_guard = int(re.findall(r'\d+', action)[0])
+        if action.startswith('f'):
+            sleep_start = int(re.findall(r'\d+', timestamp)[-1])
+        if action.startswith('w'):
+            sleep_end = int(re.findall(r'\d+', timestamp)[-1])
+            sleep_tracker[active_guard].update(range(sleep_start, sleep_end)) 
+    most_asleep, minutes = sorted(sleep_tracker.items(),
+            key = lambda x : max(x[1].values()))[-1]
+    m = minutes.most_common()[0][0]
+    print(m*most_asleep)
+
 if __name__ == '__main__':
     with open('input', 'r') as f:
         log = [line.strip() for line in f]
@@ -26,3 +42,4 @@ if __name__ == '__main__':
     # sorting chronologically
     log.sort(key = lambda x : tuple(int(i) for i in re.findall(r'\d+', x)))
     part_1(log)
+    part_2(log)
