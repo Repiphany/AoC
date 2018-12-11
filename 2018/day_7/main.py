3dc35e5b7e1bd5849cbe1b25f2668c124dc48f8d
Repiphany <45775878+Repiphany@users.noreply.github.com>
2018-12-11 17:05:47 +1300

    Solution for day 7, part 2

--- 2018/day_7/main.py
+++ 2018/day_7/main.py
@@ -12,10 +12,27 @@ def part_1(restrictions):
         completed_steps.append(s_n)
     print(''.join(completed_steps))
 
+def part_2(restrictions, n_workers = 5):
+    work_remaining = {l:60+i
+            for i, l in enumerate('ABCDEFGHIJKLMNOPQRSTUVWXYZ', start = 1)}
+    completed_steps = []
+    i = 0
+    while len(completed_steps) < 26:
+        incomplete = set(i for i, v in work_remaining.items() if v)
+        s_n = sorted([i for i in incomplete
+            if len(restrictions[i] - set(completed_steps)) == 0])[:n_workers]
+        for s in s_n:
+            work_remaining[s] -= 1
+            if not work_remaining[s]:
+                completed_steps.append(s)
+        i += 1
+    print(i)
+
 if __name__ == '__main__':
     restrictions = collections.defaultdict(set)
     with open('input', 'r') as f:
         for line in f:
             restrictions[line[36]].add(line[5])
     part_1(restrictions)
+    part_2(restrictions)
 
