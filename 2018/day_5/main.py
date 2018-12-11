3ce5ff8200b082a350fc9b27be493d1d624c5668
Repiphany <45775878+Repiphany@users.noreply.github.com>
2018-12-11 15:58:50 +1300

    Solution for day 5, part 2

--- 2018/day_5/main.py
+++ 2018/day_5/main.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+import collections
+
 def part_1(polymer):
     reacted = []
     for a in polymer:
@@ -12,7 +14,23 @@ def part_1(polymer):
             reacted.append(a)
     print(len(reacted))
 
+def part_2(polymer):
+    reacted = collections.defaultdict(list)
+    for a in polymer:
+        for letter in 'abcdefghijklmnopqrstuvwxyz':
+            if a.lower() == letter:
+                continue
+            try:
+                if abs(ord(a) - ord(reacted[letter][-1])) == 32:
+                    b = reacted[letter].pop()
+                else:
+                    reacted[letter].append(a)
+            except IndexError:
+                reacted[letter].append(a)
+    print(sorted([len(v) for v in reacted.values()])[0])
+
 if __name__ == '__main__':
     with open('input', 'r') as f:
         polymer = f.read().strip()
     part_1(polymer)
+    part_2(polymer)
