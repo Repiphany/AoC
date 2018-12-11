d2ed65b56af679a322a90971999917428fb3a76b
Repiphany <45775878+Repiphany@users.noreply.github.com>
2018-12-11 14:45:26 +1300

    Solution for day 3, part 2

--- 2018/day_3/main.py
+++ 2018/day_3/main.py
@@ -11,6 +11,18 @@ def part_1(claims):
                 claimed[(xi, yi)].append(elf)
     print(sum(1 for v in claimed.values() if len(v) >= 2))
 
+def part_2(claims):
+    claimed = collections.defaultdict(list)
+    for elf, (x, y, w, h) in claims.items():
+        for xi in range(x, x + w):
+            for yi in range(y, y + h):
+                claimed[(xi, yi)].append(elf)
+    overlaps = set()
+    for point, elves in claimed.items():
+        if len(elves) > 1:
+            overlaps.update(set(elves))
+    print(set(claims.keys()) - overlaps)
+
 if __name__ == '__main__':
     claims = {}
     with open('input', 'r') as f:
@@ -18,3 +30,4 @@ if __name__ == '__main__':
             elf, x, y, w, h = [int(i) for i in re.findall(r'\d+', line)]
             claims[elf] = (x, y, w, h)
     part_1(claims)
+    part_2(claims)
