c2bdd79f4f4191dfd1c26140b4f3c392e913ca82
Repiphany <45775878+Repiphany@users.noreply.github.com>
2018-12-11 16:49:36 +1300

    Solution for day 6, part 1

---
+++ 2018/day_6/main.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+import numpy as np
+
+def part_1(coordinates):
+    bounds = np.max(coordinates, axis = 0)
+    grid = np.empty(tuple(bounds), dtype = int)
+    for i, _ in np.ndenumerate(grid):
+        mh_dist = np.sum(np.absolute(coordinates - i), axis = 1)
+        idx = np.argsort(mh_dist)
+        if mh_dist[idx[0]] < mh_dist[idx[1]]:
+            grid[i] = idx[0]
+        else:
+            grid[i] = -1
+    edges = np.s_[0,:], np.s_[-1,:], np.s_[:,0], np.s_[:,-1]
+    infinite = set()
+    for edge in edges:
+        infinite.update(set(grid[edge]))
+    print(infinite)
+    areas = [np.sum(grid == i) for i in set(grid.flatten()) - infinite]
+    print(sorted(areas)[-1])
+    
+if __name__ == '__main__':
+    with open('input', 'r') as f:
+        coordinates = np.asarray([tuple(int(i) for i in line.split(', '))
+                for line in f])
+    part_1(coordinates)
+
