c0fd8662ac59442726e288a62da91d34671f2158
Repiphany <45775878+Repiphany@users.noreply.github.com>
2018-12-11 18:28:22 +1300

    Naive brute force solution for day 11, part 2

--- 2018/day_11/main.py
+++ 2018/day_11/main.py
@@ -22,6 +22,20 @@ def part_1(puzzle_input):
     print(np.array(np.unravel_index(np.argmax(cells), cells.shape)) + 1)
     print(np.max(cells))
 
+def part_2(puzzle_input):
+    grid = np.zeros((300, 300))
+    for i, _ in np.ndenumerate(grid):
+        grid[i] = total_power(i[0] + 1, i[1] + 1, puzzle_input)
+    totals = []
+    for size in range(1, 299):
+        cells = np.zeros((300 - size - 1, 300 - size - 1))
+        for i, _ in np.ndenumerate(cells):
+            cells[i] = np.sum(grid[i[0]:i[0]+size,i[1]:i[1]+size])
+        x, y = np.array(np.unravel_index(np.argmax(cells), cells.shape)) + 1
+        totals.append((x,y,size,np.max(cells)))
+    print(sorted(totals, key = lambda x : x[3])[-1])
+
 if __name__ == '__main__':
     puzzle_input = 9221
     part_1(puzzle_input)
+    part_2(puzzle_input)
