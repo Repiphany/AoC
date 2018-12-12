564be8430e3b0e44af2545f5342aa0548c744488
Repiphany <45775878+Repiphany@users.noreply.github.com>
2018-12-13 12:24:29 +1300

    Simplify storing the state of alive plants and using rules to determine next generation, waiting for population stabilization

--- 2018/day_12/main.py
+++ 2018/day_12/main.py
@@ -1,54 +1,64 @@
 #!/usr/bin/env python3
 
 import collections
+import itertools
 
 class Pots:
-    def __init__(self, initial):
-        self.state = collections.defaultdict(lambda : '.')
-        for i, x in enumerate(initial):
-            self.state[i] = x
-        self.rules = collections.defaultdict(lambda : '.')
+    def __init__(self, initial, rules):
+        # state stores all values that currently have a plant
+        self.state = set(i for i, v in enumerate(initial) if v == '#')
+        # store rules that cause a plant to grow
+        self.rules = set(rules)
         self.generation = 0
 
     def next(self):
-        changes = []
-        for i in range(min(self.state.keys()) - 3, max(self.state.keys()) + 3):
-            r = ''.join(self.state[j] for j in range(i - 2, i + 3))
-            changes.append((i, self.rules[r]))
-        for i, o in changes:
-            self.state[i] = o
-        # clear pots that are empty from state
-        for k, v in list(self.state.items()):
-            if v == '.':
-                del self.state[k]
+        children = set()
+        for i in set(itertools.chain(*(range(j - 2, j + 3) for j in self.state))):
+            rule = ''.join(self.get_plant(k) for k in range(i - 2, i + 3))
+            if rule in self.rules:
+                children.add(i)
+        self.state = children
         self.generation += 1
 
-    def pot_sum(self):
-        return sum(k for k, v in self.state.items() if v == '#')
+    def get_plant(self, i):
+        return '#' if i in self.state else '.'
 
+    def pot_sum(self):
+        return sum(self.state)
 
 if __name__ == '__main__':
     with open('input', 'r') as f:
         initial = f.readline().strip().split(': ')[-1]
-        pots = Pots(initial)
+        # skip empty line
         f.readline()
+        rules = []
         for line in f:
             rule, out = line.strip().split(' => ')
-            pots.rules[rule] = out
-    # part 1
+            if out == '#':
+                rules.append(rule)
+    pots = Pots(initial, rules)
     for _ in range(20):
         pots.next()
     print(pots.pot_sum())
 
     # part 2
+    # wait for plant population to stabilize
 
-    # pot sum growth stabilizes after many generations (>150)
-    for _ in range(180):
+    population = []
+    for _ in range(5):
         pots.next()
-    y_200 = pots.pot_sum()
+        population.append(len(pots.state))
+
+    while True:
+        if len(set(population[-5:])) == 1:
+            break
+        pots.next()
+        population.append(len(pots.state))
+
+    y1 = pots.pot_sum()
     pots.next()
-    y_201 = pots.pot_sum()
-    m = y_201 - y_200
-    f = lambda x : m*(x - 200)+y_200
+    y2 = pots.pot_sum()
+    m = y2 - y1
+    f = lambda x : m*(x - pots.generation) + y2
     print(int(f(50e9)))
     
