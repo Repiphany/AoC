f953fbb19ac7653436b9468e773d9aa9e78cb866
Repiphany <45775878+Repiphany@users.noreply.github.com>
2018-12-19 20:30:09 +1300

    Solution for day 19, parts 1 and 2

---
+++ 2018/day_19/main.py
@@ -0,0 +1,167 @@
+#!/usr/bin/env python3
+
+import re
+import collections
+import itertools
+import operator
+import functools
+
+primes = [2]
+
+def is_prime(n, primes = primes):
+    if n < 2:
+        return False
+    if n in primes:
+        return True
+    for p in primes:
+        if p*p > n:
+            return True
+        if not n%p:
+            return False
+    while True:
+        p = next_prime(p)
+        primes.append(p)
+        if p*p > n:
+            return True
+        if not n%p:
+            return False
+
+def next_prime(n):
+    n += 1 + n%2
+    while True:
+        if is_prime(n):
+            return n
+        n += 2
+
+def prime_factors(n):
+    c = collections.Counter()
+    p = 2
+    while True:
+        while not n%p:
+            c.update([p])
+            n //= p
+        if n == 1:
+            return c
+        p = next_prime(p)
+
+def divisors(n):
+    pf, mul = zip(*prime_factors(n).items())
+    for exps in itertools.product(*[range(n+1) for n in mul]):
+        yield functools.reduce(operator.mul, (p**e for p, e in zip(pf, exps)), 1)
+
+class Device:
+    def __init__(self, registers, ip_register = 0):
+        self.registers = registers
+        self.ip_register = ip_register
+
+    @property
+    def ip(self):
+        return self.registers[self.ip_register]
+
+    @ip.setter
+    def ip(self, value):
+        self.registers[self.ip_register] = value
+
+    def addr(self, a, b, c):
+        self.registers[c] = self.registers[a] + self.registers[b]
+        self.ip += 1
+
+    def addi(self, a, b, c):
+        self.registers[c] = self.registers[a] + b
+        self.ip += 1
+
+    def mulr(self, a, b, c):
+        self.registers[c] = self.registers[a] * self.registers[b]
+        self.ip += 1
+
+    def muli(self, a, b, c):
+        self.registers[c] = self.registers[a] * b
+        self.ip += 1
+
+    def banr(self, a, b, c):
+        self.registers[c] = self.registers[a] & self.registers[b]
+        self.ip += 1
+
+    def bani(self, a, b, c):
+        self.registers[c] = self.registers[a] & b
+        self.ip += 1
+
+    def borr(self, a, b, c):
+        self.registers[c] = self.registers[a] | self.registers[b]
+        self.ip += 1
+
+    def bori(self, a, b, c):
+        self.registers[c] = self.registers[a] | b
+        self.ip += 1
+
+    def setr(self, a, b, c):
+        self.registers[c] = self.registers[a]
+        self.ip += 1
+
+    def seti(self, a, b, c):
+        self.registers[c] = a
+        self.ip += 1
+
+    def gtir(self, a, b, c):
+        self.registers[c] = 1 if (a > self.registers[b]) else 0
+        self.ip += 1
+
+    def gtri(self, a, b, c):
+        self.registers[c] = 1 if (self.registers[a] > b) else 0
+        self.ip += 1
+
+    def gtrr(self, a, b, c):
+        self.registers[c] = 1 if (self.registers[a] > self.registers[b]) else 0
+        self.ip += 1
+
+    def eqir(self, a, b, c):
+        self.registers[c] = 1 if (a == self.registers[b]) else 0
+        self.ip += 1
+
+    def eqri(self, a, b, c):
+        self.registers[c] = 1 if (self.registers[a] == b) else 0
+        self.ip += 1
+
+    def eqrr(self, a, b, c):
+        self.registers[c] = 1 if (self.registers[a] == self.registers[b]) else 0
+        self.ip += 1
+
+    def run_instruction(self, program):
+        try:
+            opcode, a, b, c = program[self.ip]
+            getattr(self, opcode)(a, b, c)
+            return True
+        except IndexError:
+            return False
+
+if __name__ == '__main__':
+    program = []
+    with open('input', 'r') as f:
+        declaration = f.readline().strip()
+        ip = int(declaration[-1])
+        for i, line in enumerate(f):
+            opcode = line.split(' ')[0]
+            a, b, c = [int(i) for i in re.findall(r'\d+', line)]
+            program.append((opcode, a, b, c))
+
+    # part 1
+    device = Device(registers = [0,0,0,0,0,0], ip_register = ip)
+    r = set([0])
+    # run program until it reaches first loop
+    while True:
+        device.run_instruction(program)
+        if device.ip in r:
+            break
+        r.add(device.ip)
+    print(sum(divisors(max(device.registers))))
+
+    # part 2
+    device = Device(registers = [1,0,0,0,0,0], ip_register = ip)
+    r = set([0])
+    # run program until it reaches first loop
+    while True:
+        device.run_instruction(program)
+        if device.ip in r:
+            break
+        r.add(device.ip)
+    print(sum(divisors(max(device.registers))))
