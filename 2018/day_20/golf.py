3b3332a3c2cf6df6d6a5e95df9c836eebf7d0a5d
Repiphany <45775878+Repiphany@users.noreply.github.com>
2018-12-21 11:32:19 +1300

    Further whitespace trimming

--- 2018/day_20/golf.py
+++ 2018/day_20/golf.py
@@ -4,18 +4,11 @@ m={}
 d=n=x=y=t=0
 while 1:
  if t:x,y,n,t=b[d]
- n=m[x,y]=min(m.get((x,y),9e9),n)
- c=sys.stdin.read(1)
+ n=m[x,y]=min(m.get((x,y),9e9),n);c=sys.stdin.read(1)
  if c=='$':break
- if c=='(':
-  d+=1
-  b[d]=x,y,n,0
- d-=c==')'
- t=c=='|'
- y+={'N':1,'S':-1}.get(c,0)
- x+={'E':1,'W':-1}.get(c,0)
- n+=1
+ if c=='(':d+=1;b[d]=x,y,n,0
+ d-=c==')';t=c=='|';y+={'N':1,'S':-1}.get(c,0);x+={'E':1,'W':-1}.get(c,0);n+=1
 p=print
 v=m.values()
 p(max(v))
-p(sum([x>999 for x in v]))
+p(sum(x>999 for x in v))
