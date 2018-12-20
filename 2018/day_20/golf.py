d70b0c785fd43d6eb20fc182e142c271e9c7fa81
Repiphany <45775878+Repiphany@users.noreply.github.com>
2018-12-21 11:36:41 +1300

    open(0) instead of import sys;sys.stdin.read(1)

--- 2018/day_20/golf.py
+++ 2018/day_20/golf.py
@@ -1,10 +1,10 @@
-import sys
 b={}
 m={}
 d=n=x=y=t=0
+s=open(0)
 while 1:
  if t:x,y,n,t=b[d]
- n=m[x,y]=min(m.get((x,y),9e9),n);c=sys.stdin.read(1)
+ n=m[x,y]=min(m.get((x,y),9e9),n);c=s.read(1)
  if c=='$':break
  if c=='(':d+=1;b[d]=x,y,n,0
  d-=c==')';t=c=='|';y+={'N':1,'S':-1}.get(c,0);x+={'E':1,'W':-1}.get(c,0);n+=1
