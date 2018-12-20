b6ce3b49d39c0fa1f8c12c30ae64c962bbde5763
Repiphany <45775878+Repiphany@users.noreply.github.com>
2018-12-20 20:12:39 +1300

    Simplified mapping logic to correct error in branching cases

--- 2018/day_20/main.py
+++ 2018/day_20/main.py
@@ -2,54 +2,42 @@
 
 import collections
 
-if __name__ == '__main__':
-    known = collections.defaultdict(set)
-    min_steps = collections.defaultdict(lambda : float('inf'))
-    branches = {}
-    cache = []
-    def step(steps):
-        branch = n = 0
+class Facility:
+    def __init__(self):
+        self.min_steps = collections.defaultdict(lambda : float('inf'))
+
+    def step(self, directions):
+        branches = {}
+        branch = n = x = y = 0
         branched = False
-        x, y = (0, 0)
         while True:
             if branched:
                 x, y, n = branches[branch]
                 branched = False
-            n = min_steps[(x, y)] = min(min_steps[(x, y)], n)
-
-            if cache:
-                c = cache.pop()
-            else:
-                c = next(steps)
+            n = self.min_steps[(x, y)] = min(self.min_steps[(x, y)], n)
+            c = next(directions)
             if c == '$':
                 return
             if c == '(':
                 branch += 1
                 branches[branch] = (x, y, n)
-                continue
             if c == ')':
                 branch -= 1
-                continue
             if c == '|':
-                cn = next(steps)
-                if cn == ')':
-                    branch -= 1
-                    continue
-                else:
-                    cache.append(cn)
                 branched = True
-                continue
             if c == 'N':
                 y += 1
-            if c == 'E':
-                x += 1
             if c == 'S':
                 y -= 1
+            if c == 'E':
+                x += 1
             if c == 'W':
                 x -= 1
             n += 1
-    
+
+if __name__ == '__main__':
+    facility = Facility()
     with open('input', 'r') as f:
-        step(iter(f.read()))
-    print(max(min_steps.items(), key = lambda x : x[1])[1])
-    print(len([k for k, v in min_steps.items() if v >= 1000]))
+        facility.step(iter(f.read()))
+    print(max(facility.min_steps.items(), key = lambda x : x[1])[1])
+    print(len([k for k, v in facility.min_steps.items() if v >= 1000]))
