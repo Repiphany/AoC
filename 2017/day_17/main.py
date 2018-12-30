64151fe43a75fa8a6cd978064eb22da2281db9ae
Repiphany <45775878+Repiphany@users.noreply.github.com>
2018-12-30 23:38:37 +1300

    Smarter way to track value after zero without storing entire ring

--- 2017/day_17/main.py
+++ 2017/day_17/main.py
@@ -7,15 +7,22 @@ def spinlock(step, N):
     ring = collections.deque([])
     zero = set()
     for i in range(N + 1):
-        if not i%100000:
-            print(i)
         ring.rotate(-step)
         ring.append(i)
-        #az = ring[(ring.index(0) + 1)%len(ring)]
-        #if az not in zero:
-        #    print(i, az)
-        #    zero.add(az)
     return ring
+
+def spinlock_zero(step, N):
+    zero_index = 0
+    zero_n = 0
+    pos = 0
+    for i in range(1, N + 1):
+        pos = (pos + step)%(i) + 1
+        if pos == zero_index + 1:
+            zero_n = i
+        if pos <= zero_index:
+            zero_index += 1
+    return zero_n
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -23,6 +30,6 @@ if __name__ == '__main__':
     args = parser.parse_args()
     ring = spinlock(args.INPUT, 2017)
     print(ring[0])
-    ring = spinlock(args.INPUT, 50000000)
-    print(ring[ring.index(0) + 1])
+    zn = spinlock_zero(args.INPUT, 50000000)
+    print(zn)
 
