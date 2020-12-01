625276e0c6db8250b17057447c24a42739b1539e
Repiphany <45775878+Repiphany@users.noreply.github.com>
2020-12-01 14:32:09 +1300

    Fix syntax error

--- lib/aoc/year_2019_day_04.py
+++ lib/aoc/year_2019_day_04.py
@@ -5,22 +5,22 @@ from .input import get_input
 import itertools
 
 def valid(password):
-    return all[
+    return all([
             password > 99999,
             password < 1000000,
             len(list(itertools.groupby(str(password)))) < 6,
             str(password) == ''.join(sorted(str(password)))
-            ]
+            ])
 
 def valid2(password):
     grouper = [(a, list(b)) for a, b in itertools.groupby(str(password))]
-    return all[
+    return all([
             password > 99999,
             password < 1000000,
             len(grouper) < 6,
             any(len(b) == 2 for a, b in grouper),
             str(password) == ''.join(sorted(str(password))),
-            ]
+            ])
 
 def test(args):
     assert valid(111111)
